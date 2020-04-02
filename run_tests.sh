75beda3f98809bb93547ec146a88ef15563b6388
Nils Gangby <nils.gangby@infotiv.se>
2020-04-02 16:42:32 +0200

    jenkins push

--- run_tests.sh
+++ run_tests.sh
@@ -2,4 +2,4 @@
 
 python -m pytest --junitxml=xml/python_tests.xml -q --cmdopt=type2 tests/space_detection_tests.py
 pycodestyle --exclude PyRoboViz,car_controller,lidarServer . > xml/pep8.log
-printf '%d\n' $?
+# printf '%d\n' $?
